1056155f8428a2177ed6b7c93749f48c814f4c79
Ryan O'Beirne <robeirne@spoonflower.com>
2019-12-10 11:46:30 -0500

    Add thousands of words

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,27 @@ use std::env::args;
 mod noun;
 mod adjective;
 
+use noun::NOUNS;
+use adjective::ADJECTIVES;
+
 fn main() -> Result<(), Error> {
     let count = args()
-        .nth(1).unwrap_or(String::from("1"))
+        .nth(1).unwrap_or_else(|| String::from("1"))
         .parse::<usize>().unwrap_or(1);
 
+    let std_out = &mut stdout();
     let rng = &mut thread_rng();
 
     for _i in 0..count {
-        writeln!(stdout(), "{}", three_words(rng))?
+        writeln!(std_out, "{}", three_words(rng))?
     }
 
     Ok(())
 }
 
 fn three_words(rng: &mut ThreadRng) -> String {
-    let adjs: Vec<&str> = adjective::ADJECTIVES.choose_multiple(rng, 2)
-        .map(|s| (*s).into())
-        .collect();
-
-    let noun = noun::NOUNS.choose(rng).unwrap();
+    let adjs = ADJECTIVES.choose_multiple(rng, 2).collect::<Vec<_>>();
+    let noun = NOUNS.choose(rng).expect("Slice is empty!");
 
     format!("{} {} {}", adjs[0], adjs[1], noun)
 }
